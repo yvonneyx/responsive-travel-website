c4fa7be45974f3fc2974771eeed08b5ef718791a
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-05 22:33:38 +0200

    Scroll Sections Active Link

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,3 +86,27 @@ function scrollUp() {
 
 window.addEventListener("scroll", scrollUp);
 
+/* ============= SCROLL SECTIONS ACTIVE LINK ============ */
+const sections = document.querySelectorAll("section[id]");
+
+function scrollAvtive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__item a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__item a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollAvtive);
