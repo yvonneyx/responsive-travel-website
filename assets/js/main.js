d6d5ad8cdbb9e285e8f95fb39c8b0340b8ef6c43
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-05 17:27:59 +0200

    Change Background Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,11 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* ============= CHANGE BACKGROUND HEADER ============ */
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
