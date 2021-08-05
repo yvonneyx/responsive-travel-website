9fcf18e128d206a1113e84faeaa9ebb097fcefea
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-05 23:10:00 +0200

    Dark/Light Theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,3 +110,37 @@ function scrollAvtive() {
 }
 
 window.addEventListener("scroll", scrollAvtive);
+
+/* ============ DARK LIGHT THEME ============ */
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () => {
+  return document.body.classList.contains(darkTheme) ? "dark" : "light";
+};
+const getCurrentIcon = () => {
+  return themeButton.classList.contains(iconTheme)
+    ? "ri-moon-line"
+    : "ri-sun-line";
+};
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
