6e2c1c6fc9e973184c08179307177f91d6fda40f
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-05 22:12:35 +0200

    Scroll Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,13 @@ function finalVideo() {
 }
 
 videoFile.addEventListener("ended", finalVideo);
+
+/* ============= SHOW SCROLL UP ============ */
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollUp);
+
